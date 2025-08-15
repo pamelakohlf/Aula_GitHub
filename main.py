bfeb726821f5c58247868e530152ccdc217efd75
Pâmela <pamelakohlf@gmail.com>
2025-08-14 21:40:13 -0300

    enviando alterações do arquivo

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Òla mundo")
+
+print("Bem vindo ao sistema")
+
+print("Não aguento mais, quase sexta")
+
+
---
+++ newfile.py
@@ -0,0 +1,10 @@
+
+x = 10
+y = 20
+def soma(a,b):
+	return a+b
+
+res = soma(x,y)
+
+print(res)
+
